5542616c9917f08d80b6893185803071b030d728
Evan Dawson <Zormion@gmail.com>
2017-02-12 16:42:38 -0500

    Cleared it all for new code

--- README.md
+++
@@ -1,2 +0,0 @@
-# 2017 Robot
-Robot code for the 2017 First Robotics Competition
--- src/org/usfirst/frc/team5483/robot/toolbox/Settings.java
+++
@@ -1,8 +0,0 @@
-package org.usfirst.frc.team5483.robot.toolbox;
-
-public class Settings {
-	public static boolean useGyro = true;
-	public static boolean useEncoders = false;
-	
-	public static boolean omniWheels = false;
-}
