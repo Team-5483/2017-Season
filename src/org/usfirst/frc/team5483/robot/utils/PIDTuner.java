e8cb0c15418599066c974e9bb5aecda6442da36f
Evan Dawson <zormion@gmail.com>
2017-03-12 15:01:52 -0400

    fixed pid tuner

--- src/org/usfirst/frc/team5483/robot/utils/PIDTuner.java
+++ src/org/usfirst/frc/team5483/robot/utils/PIDTuner.java
@@ -1,16 +1,18 @@
 package org.usfirst.frc.team5483.robot.utils;
 
 public class PIDTuner {
+	
+	private double accumError = 0;
+	private double lastError = 0;
+	
 	public double pidTuned(double setpoint, double current, 
 			double Kp, double Ki, double Kd) {
 		
 		double error = setpoint - current;
-		double P = Kp * error;
 		
-		static double accumError = 0;
+		double P = Kp * error;
 		accumError += error;
 		double I = Ki * accumError;
-		static double lastError = 0;
 		double errorDiff = error - lastError;
 		lastError = error;
 		double D = Kd * errorDiff;
